91edb2beae1647d10c5b2fce1b97064370fd3f8f
BhadraMV <124436190+BhadraMV@users.noreply.github.com>
2023-02-22 22:22:22 +0530

    Add files via upload

---
+++ week3Day2Assignment/Automation.java
@@ -0,0 +1,30 @@
+package week3.day2Assignments;
+
+public class Automation extends MultipleLangauge{
+
+	public void java() {
+		System.out.println("Java");
+		
+	}
+
+	public void Selenium() {
+		System.out.println("Selenium");
+		
+	}
+
+	public void ruby() {
+		System.out.println("Ruby");
+		
+	}
+	public static void main(String[] args) {
+		
+		Automation au=new Automation();
+		au.java();
+		au.python();
+		au.ruby();
+		au.Selenium();
+	}
+
+
+	
+}
---
+++ week3Day2Assignment/TestTool.java
@@ -0,0 +1,6 @@
+package week3.day2Assignments;
+
+public interface TestTool {
+
+	public void Selenium();
+}
